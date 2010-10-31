85501037d71cd7d64afff4c185befea1eed6b57c
tobaljinder <tobaljinder@2faa8518-8139-5a18-627d-c5d49b08a286>
2010-10-31 05:23:40 +0000

    Added support for injecting method binding to be executed in case of some special operations

--- branches/presenter-faces-sota/src/main/java/org/baljinder/presenter/dataacess/jsf/SupportsJsfComponent.java
+++ branches/presenter-faces-sota/src/main/java/org/baljinder/presenter/dataacess/jsf/SupportsJsfComponent.java
@@ -1,6 +1,14 @@
 package org.baljinder.presenter.dataacess.jsf;
 
+import java.util.Map;
+
+import org.baljinder.presenter.dataacess.IDataController;
+
 
 public interface SupportsJsfComponent {
+	
+	public void setActionMethodBinding(Map<IDataController.Operation,String> actionMethodBinding) ;
+	
+	public Map<IDataController.Operation,String> getActionMethodBinding() ;
 	
 }
