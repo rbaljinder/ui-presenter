3946447b235049ab19a63509413b378343cc06a4
tobaljinder <tobaljinder@2faa8518-8139-5a18-627d-c5d49b08a286>
2010-10-31 05:24:58 +0000

    Modified demo to capture all changes

---
+++ branches/presenter-demo-sota/src/main/java/org/baljinder/presenter/testing/support/model/Transaction.java
@@ -0,0 +1,26 @@
+package org.baljinder.presenter.testing.support.model;
+
+public class Transaction {
+
+	private Long transactionId;
+	
+	private String decision ;
+	
+	public Long getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getDecision() {
+		return decision;
+	}
+
+	public void setDecision(String decision) {
+		this.decision = decision;
+	}
+
+
+}
