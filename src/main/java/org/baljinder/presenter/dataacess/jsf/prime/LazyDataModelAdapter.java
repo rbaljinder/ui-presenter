dc58f7c45dade2038f77eab5d894df53d7bf6939
tobaljinder <tobaljinder@2faa8518-8139-5a18-627d-c5d49b08a286>
2010-11-27 20:12:23 +0000

    added support for prime faces components

---
+++ src/main/java/org/baljinder/presenter/dataacess/jsf/prime/LazyDataModelAdapter.java
@@ -0,0 +1,32 @@
+package org.baljinder.presenter.dataacess.jsf.prime;
+
+import java.util.List;
+import java.util.Map;
+
+import org.primefaces.model.LazyDataModel;
+
+public class LazyDataModelAdapter extends LazyDataModel<Map<String, Object>>{
+
+	private static final long serialVersionUID = 1L;
+	
+	private PrimeFacesSupportingDataController dataController;
+
+	
+	public LazyDataModelAdapter(
+			PrimeFacesSupportingDataController primeFacesSupportingDataController) {
+		dataController = primeFacesSupportingDataController;
+	}
+
+
+	@Override
+	public List<Map<String, Object>> load(int first, int pageSize, String sortField, boolean sortOrder, Map<String, String> arg4) {
+
+		return null;
+	}
+	
+	@Override
+	public int getRowCount() {
+		return dataController.getDao().getCountOfRecords(dataController.getCountQuery());
+	}
+
+}
