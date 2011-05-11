ac4c8f2f0727a2a9b3730d0229b4c8c788ab93a7
tobaljinder <tobaljinder@2faa8518-8139-5a18-627d-c5d49b08a286>
2011-05-11 16:59:36 +0000

    sync with uncommitted local changes

--- trunk/presenter/presenter-faces/src/main/java/org/baljinder/presenter/dataacess/jsf/prime/PrimeFacesSupportingDataController.java
+++ trunk/presenter/presenter-faces/src/main/java/org/baljinder/presenter/dataacess/jsf/prime/PrimeFacesSupportingDataController.java
@@ -7,23 +7,70 @@ import org.baljinder.presenter.dataacess.jsf.SupportsJsfComponent;
 import org.primefaces.model.LazyDataModel;
 
 //TODO : implement methods
-public class PrimeFacesSupportingDataController extends FacesDataController implements SupportsJsfComponent {
+public class PrimeFacesSupportingDataController extends FacesDataController
+		implements SupportsJsfComponent {
 
-	private LazyDataModelAdapter dataModelAdapter = new LazyDataModelAdapter(this);
+	private LazyDataModelAdapter dataModelAdapter = new LazyDataModelAdapter(
+			this);
 	
-	public LazyDataModel<Map<String, Object>> getDataModel(){
+	public static enum DisplayMode {LIST,UPDATE,SHOW,CREATE} ;
+	
+	private DisplayMode displayMode = DisplayMode.LIST;
+	
+	public String getDisplayMode() {
+		return displayMode.toString();
+	}
+
+	//Helper method to fool jsf.
+	public void updateDisplayMode(){
+		
+	}
+	public void setDisplayMode(DisplayMode displayMode) {
+		this.displayMode = displayMode;
+	}
+	
+	public void setDisplayMode(String displayMode) {
+		this.displayMode = DisplayMode.valueOf(displayMode);
+	}
+	
+
+	public LazyDataModel<Map<String, Object>> getDataModel() {
 		return dataModelAdapter;
 	}
 
 	@Override
 	protected String selectDataFromTable() {
-		//throw new UnsupportedOperationException("Not implemented yet");
+		// throw new UnsupportedOperationException("Not implemented yet");
 		return "";
 	}
 
 	@Override
 	protected void clearSelectedIndexesOfUITable() {
-		//throw new UnsupportedOperationException("Not implemented yet");
+		// throw new UnsupportedOperationException("Not implemented yet");
 	}
+//
+//	@Override
+//	public String create() {
+//		setDisplayMode(DisplayMode.CREATE); //is there any way else just on client side
+//		return super.create();
+//	}
+//	
+//	@Override
+//	public String delete() {
+//		setDisplayMode(DisplayMode.LIST); 
+//		return super.delete();
+//	}
+//	
+//	@Override
+//	public String deleteSelectedElements() {
+//		setDisplayMode(DisplayMode.LIST); //is there any way else just on client side
+//		return super.deleteSelectedElements();
+//	}
+//	
+//	@Override
+//	public String update() {
+//		setDisplayMode(DisplayMode.LIST); //is there any way else just on client side
+//		return super.update();
+//	}
 	
 }
