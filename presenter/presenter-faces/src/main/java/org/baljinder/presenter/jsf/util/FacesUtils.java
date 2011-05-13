52340b475f8682a401362f241dd7baaee79aba31
tobaljinder <tobaljinder@2faa8518-8139-5a18-627d-c5d49b08a286>
2011-05-13 03:02:14 +0000

    sync

--- presenter/presenter-faces/src/main/java/org/baljinder/presenter/jsf/util/FacesUtils.java
+++ presenter/presenter-faces/src/main/java/org/baljinder/presenter/jsf/util/FacesUtils.java
@@ -1,16 +1,10 @@
 package org.baljinder.presenter.jsf.util;
+
 /**
  * 
  */
 
-
 import java.util.Set;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.myfaces.trinidad.bean.FacesBean;
-import org.apache.myfaces.trinidad.bean.PropertyKey;
-import org.apache.myfaces.trinidad.component.UIXTable;
 
 /**
  * @author Baljinder Randhawa
@@ -20,9 +14,7 @@ public class FacesUtils {
 
 	public static final String _ROW_SELECTION_PROP_KEY = "rowSelection";
 
-	private static Log logger = LogFactory.getLog(FacesUtils.class);
-	
-	static MessageBean messageBean ;
+	static MessageBean messageBean;
 
 	public static MessageBean getMessageBean() {
 		return messageBean;
@@ -40,43 +32,4 @@ public class FacesUtils {
 		messageBean.addErrorMessage(message);
 	}
 
-	public static boolean isMultiSelect(UIXTable table) {
-		Object value = getPropertyValue(_ROW_SELECTION_PROP_KEY, table);
-		if (value != null)
-			return "multiple".equalsIgnoreCase(value.toString());
-		return false;
-	}
-
-	public static boolean isSingleSelect(UIXTable table) {
-		Object value = getPropertyValue(_ROW_SELECTION_PROP_KEY, table);
-		if (value != null)
-			return "single".equalsIgnoreCase(value.toString());
-		return false;
-	}
-
-	public static PropertyKey getProperty(String key, UIXTable table) {
-		if (key == null || table == null)
-			return null;
-		FacesBean facesBean = table.getFacesBean();
-		Set<PropertyKey> keys = facesBean.keySet();
-		for(PropertyKey propertyKey :keys){
-			if (key.equalsIgnoreCase(propertyKey.getName())) {
-				if (logger.isDebugEnabled())
-					logger.debug("Found UI table property " + key);
-				return propertyKey;
-			}
-		}
-		return null;
-	}
-
-	public static Object getPropertyValue(String key, UIXTable table) {
-		if ((table != null) && (key != null)) {
-			FacesBean facesBean = table.getFacesBean();
-			PropertyKey propKey = getProperty(key, table);
-			if (propKey != null) {
-				return facesBean.getProperty(propKey);
-			}
-		}
-		return null;
-	}
 }
